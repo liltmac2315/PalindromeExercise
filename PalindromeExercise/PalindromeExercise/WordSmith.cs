4d429795fe4daabbee71e4b3232818363e6d788f
tommy santos <tommy1santos@yahoo.com>
2024-02-23 16:31:57 -0500

    Palindrome Exercise completed.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleForPalindrome
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word) 
+        {
+            //reverse the word 
+            var reversed = "";
+
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            if (reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else 
+            { 
+                return false;
+            }
+
+            //var reversedword = word.Reverse();
+            //return word.ToLower().SequenceEqual(reversedword);
+
+            //var reversed = word.Reverse();
+
+            //if(reversed.ToString() == word) 
+            //{
+               //return true;
+            //}
+            //else 
+            //{
+                //return false;
+            //}
+
+            //compare the original with the reversed
+
+            //return wether they are the same or not
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using ExampleForPalindrome;
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar",true)]
+        [InlineData("mom",true)]
+        public void MyTest(string word, bool expected) 
+        { 
+            //arrange
+            var tester = new WordSmith();
+            //act
+            var actual = tester.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
